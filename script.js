5f1aedef564fc704f21277f7cccadc0fd24015a6
s4dnie <chewbee2009@gmail.com>
2024-12-08 21:23:18 -0600

    Deleted second page Styled website Added timer; is not functional yet Added instructions on how to clean

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const form = document.getElementById("form-id");
+const inst = document.getElementById("instructions");
+
+inst.style.display = "none";
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -9,10 +12,19 @@ form.addEventListener("submit", (event) => {
 
     var boxesSelect = document.querySelectorAll("input[type='checkbox']:checked");
     const selectedValues = Array.from(boxesSelect).map(checkbox => checkbox.value);
- 
+
+    console.log(selectedValues);
+
     if (selectedValues <= 0) {
         alert("You must select at least one area!");
-    }else {
-        window.location.href = "program.html";
+    } else {
+        inst.style.display = "block";
+
+        let check_container = document.getElementById("startup")
+        check_container.style.display = "none";
+
+        inst.querySelector("span").innerHTML = "You will now work on: the " + selectedValues[0].toString();
+        document.getElementById("do-not-attempt").innerHTML = "Do not attempt to clean anything other than the " + selectedValues[0].toString();
     }
 });
+
