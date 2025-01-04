5cb51b28a9865f0cda144831518e2b698f900001
s4dnie <chewbee2009@gmail.com>
2025-01-04 15:21:48 -0600

    CSS overhaul Added timer functionality Accessed jQuery Learned + Incorporated CSS Grid and Flexbox

--- script.js
+++ script.js
@@ -1,30 +1,72 @@
-const form = document.getElementById("form-id");
-const inst = document.getElementById("instructions");
+$("#program").hide();
 
-inst.style.display = "none";
+const form = document.getElementById("form-id");
+const timeDisplay = document.querySelector(".timer");
+const button = document.getElementById("start");
+
+
+
+function getValues() {
+    let timeselect;
+    let breaklength;
+    let boxesSelect;
+    let selectedValues;
+
+    timeselect = document.getElementById("select"); //get dropdown menu
+    var x = timeselect.selectedIndex; //get selected option
+    var y = timeselect.options; //get all options
+    breaklength = y[x].value;
+
+    boxesSelect = document.querySelectorAll("input[type='checkbox']:checked"); //node list of checked boxes
+    selectedValues = Array.from(boxesSelect).map(checkbox => checkbox.value); //turn checked boxes into an array
+
+    return [selectedValues, breaklength];
+}
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    values = getValues();
 
-    const timeselect = document.getElementById("select"); //get dropdown menu
-    var x = timeselect.selectedIndex; //get selected option
-    var y = timeselect.options; //get all options
-
-    var boxesSelect = document.querySelectorAll("input[type='checkbox']:checked");
-    const selectedValues = Array.from(boxesSelect).map(checkbox => checkbox.value);
-
-    console.log(selectedValues);
-
-    if (selectedValues <= 0) {
+    if (values[0] <= 0) {
         alert("You must select at least one area!");
     } else {
-        inst.style.display = "block";
-
-        let check_container = document.getElementById("startup")
-        check_container.style.display = "none";
-
-        inst.querySelector("span").innerHTML = "You will now work on: the " + selectedValues[0].toString();
-        document.getElementById("do-not-attempt").innerHTML = "Do not attempt to clean anything other than the " + selectedValues[0].toString();
+        $("#program").show()
+        $("#info").hide();
+        $("#startup").hide();
+        $("#subtitle").hide();
+        cyclethrough();
     }
 });
 
+function cyclethrough() {
+    function timer(seconds) {
+        let countdown;
+        const now = Date.now();
+        const then = now + seconds * 1000;
+        displayTimeLeft(seconds);
+
+        countdown = setInterval(() => {
+            const secondsLeft = Math.round((then - Date.now()) / 1000);
+            if (secondsLeft < 0) {
+                
+                clearInterval(countdown);
+                return;
+            }
+            displayTimeLeft(secondsLeft);
+        }, 1000);
+    }
+
+    function displayTimeLeft(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainderSeconds = seconds % 60;
+        const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+
+        timeDisplay.textContent = display;
+    }
+
+    for (let i = 0; i < values[0].length; i++) {
+        button.addEventListener("click", function () {
+            timer(300);
+        })
+    }
+}
